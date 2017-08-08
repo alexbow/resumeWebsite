be7de63b23f4573da932976523b5d5b38d0c65b4
Alex Bowen <mab2098@g.rit.edu>
2017-08-07 19:27:21 -0500

    Added terminal styling and lightbulb button

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,9 +3,11 @@ function switchStyleSheet(){
 	if(currentStyleSheet != null){
 		document.getElementById('myStyleSheet').setAttribute('href', './CSS/stylesheet2.css');
 		document.getElementById('myStyleSheet').id = 'myStyleSheet2';
+		document.getElementById('lightbulb').setAttribute('src', './Resources/lightbulbWhite.png');
 	}
 	else{
 		document.getElementById('myStyleSheet2').setAttribute('href', './CSS/stylesheet1.css');
 		document.getElementById('myStyleSheet2').id = 'myStyleSheet';
+		document.getElementById('lightbulb').setAttribute('src', './Resources/lightbulbBlack.png');
 	}
 }
