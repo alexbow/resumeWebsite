a43963f0b543ea55f6ee6c61fdf5bc089f00197b
Alex Bowen <mab2098@g.rit.edu>
2017-08-10 12:02:17 -0500

    Added styling to the menu bar and console fix

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,10 +4,12 @@ function switchStyleSheet(){
 		document.getElementById('myStyleSheet').setAttribute('href', './CSS/stylesheet2.css');
 		document.getElementById('myStyleSheet').id = 'myStyleSheet2';
 		document.getElementById('lightbulb').setAttribute('src', './Resources/lightbulbWhite.png');
+		document.getElementById('portfolio').setAttribute('src', './Resources/portfolioWhite.gif');
 	}
 	else{
 		document.getElementById('myStyleSheet2').setAttribute('href', './CSS/stylesheet1.css');
 		document.getElementById('myStyleSheet2').id = 'myStyleSheet';
 		document.getElementById('lightbulb').setAttribute('src', './Resources/lightbulbBlack.png');
+		document.getElementById('portfolio').setAttribute('src', './Resources/portfolioBlack.gif');
 	}
 }
