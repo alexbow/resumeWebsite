3a8a306c788fa122f11162c985ec416ec48bb6b5
Alex Bowen <Alex Bowen>
2017-09-18 21:59:07 -0400

    fixed menu css

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,8 @@
 var styleSheetNumber = 1;
 
+/**
+* switches between the two stylesheets when the lightbulb icon is pressed
+*/
 function switchStyleSheet(){
 	var currentStyleSheet = document.getElementById('myStyleSheet');
 	if(currentStyleSheet != null){
@@ -7,39 +10,21 @@ function switchStyleSheet(){
 		styleSheetNumber = 2;
 		document.getElementById('myStyleSheet').setAttribute('href', './CSS/stylesheet2.css');
 		document.getElementById('myStyleSheet').id = 'myStyleSheet2';
-		document.getElementById('lightbulb').setAttribute('src', './Resources/lightbulbWhite.png');
-		document.getElementById('portfolio').setAttribute('src', './Resources/portfolioWhite.gif');
-		// document.getElementById('logoPicture').setAttribute('src', './Resources/logoRedTop.gif');
-		document.getElementById('resume').setAttribute('src', './Resources/resumeWhite.gif');
-		document.getElementById('contact').setAttribute('src', './Resources/mailWhite.gif');
-		document.getElementById('experience').setAttribute('src', './Resources/experienceWhite.gif');
-		document.getElementById('skills').setAttribute('src', './Resources/computerWhite.gif');
 	}
 	else{
 		localStorage.setItem("style", 1);
 		styleSheetNumber = 1;
 		document.getElementById('myStyleSheet2').setAttribute('href', './CSS/stylesheet1.css');
 		document.getElementById('myStyleSheet2').id = 'myStyleSheet';
-		document.getElementById('lightbulb').setAttribute('src', './Resources/lightbulbBlack.png');
-		document.getElementById('portfolio').setAttribute('src', './Resources/portfolioBlack.gif');
-		// document.getElementById('logoPicture').setAttribute('src', './Resources/logoBlueTop.gif');
-		document.getElementById('resume').setAttribute('src', './Resources/resumeBlack.gif');
-		document.getElementById('contact').setAttribute('src', './Resources/mailBlack.gif');
-		document.getElementById('experience').setAttribute('src', './Resources/experienceBlack.gif');
-		document.getElementById('skills').setAttribute('src', './Resources/computerBlack.gif');
 	}
 }
 
+/**
+* maintains whichever stylesheet you had on another page for a more immersive experience
+*/
 function persistentStyles(){
 	if(localStorage.getItem("style") != '1'){
 		document.getElementById('myStyleSheet').setAttribute('href', './CSS/stylesheet2.css');
 		document.getElementById('myStyleSheet').id = 'myStyleSheet2';
-		document.getElementById('lightbulb').setAttribute('src', './Resources/lightbulbWhite.png');
-		document.getElementById('portfolio').setAttribute('src', './Resources/portfolioWhite.gif');
-		document.getElementById('logoPicture').setAttribute('src', './Resources/logoRedTop.gif');
-		document.getElementById('resume').setAttribute('src', './Resources/resumeWhite.gif');
-		document.getElementById('contact').setAttribute('src', './Resources/mailWhite.gif');
-		document.getElementById('experience').setAttribute('src', './Resources/experienceWhite.gif');
-		document.getElementById('skills').setAttribute('src', './Resources/computerWhite.gif');
 	}
 }
